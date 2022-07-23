dadd0facde0f6a37acb41487adfbda7af95021bb
ababaei <kzmmaz@gmail.com>
2022-07-23 17:41:37 +0200

    issues solved !

--- srcs/wordpress/init.sh
+++ srcs/wordpress/init.sh
@@ -8,10 +8,6 @@ else
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp
 
-	if [[ ! -d /var/www/html ]];
-	then
-		mkdir /var/www/html
-	fi
 	cd /var/www/html
 
 	wp --allow-root core download
@@ -21,7 +17,7 @@ else
 	done
 
 	wp --allow-root config create --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}"	--dbpass="${MYSQL_PASSWORD}" --dbhost="${MYSQL_HOSTNAME}" --dbcharset="utf8" --dbcollate="utf8_general_ci"
-	wp --allow-root core install --url=ababaei.42.fr/wordpress --title=ababaei --admin_user="${WP_ADMIN}" --admin_password="${WP_ADPASS}" --admin_email="${WP_MAIL}" --skip-email
+	wp --allow-root core install --url=ababaei.42.fr --title=ababaei --admin_user="${WP_ADMIN}" --admin_password="${WP_ADPASS}" --admin_email="${WP_MAIL}" --skip-email
 	wp --allow-root user create "${WP_USER}" "${WP_USERMAIL}" --role=author --user_pass="${WP_USERPASS}"
 	wp --allow-root theme install "twentysixteen" --activate
 
